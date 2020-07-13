bf26f0194c6da9f535c841dc8f93ee21c8c6dc98
Cheshire <master@cheshire.chat>
2020-07-13 18:28:26 +0200

    seed users

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -11,6 +13,12 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            'name' => 'master',
+            'email' => 'master@cheshire.chat',
+            'password' => Hash::make('satomi'),
+        ]);
+
         factory(App\User::class, 50)->create();
     }
 }
