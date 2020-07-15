e16f9aa16a8d708436280a87edc2aefe4f5a9358
Cheshire <master@cheshire.chat>
2020-07-15 12:28:11 +0200

    fix user synchronization

--- resources/js/store/modules/auth.js
+++ resources/js/store/modules/auth.js
@@ -10,34 +10,36 @@ const actions = {
     login: function ({ commit }, credentials) {
         authService.login(credentials)
             .then(response => {
-                if (response.status === 302) {
-                    console.log("here")
-                } else {
-                    commit('setAuthenticatedUser', response.data);
-                    window.location.href="/"
-                }
+                commit('setAuthenticatedUser', response.data);
+                window.location.href = "/"
             });
     },
 
-    synchronize: function({ commit }) {
+    synchronize: function ({ commit }) {
         authService.synchronize()
             .then(response => {
-                commit('setAuthenticatedUser', response.data)
+                if (Object.keys(response.data).length === 0 && response.data.constructor === Object) {
+                    commit('removeAuthenticatedUser')
+                } else {
+                    commit('setAuthenticatedUser', response.data)
+                }
             })
-    }
+    },
 }
 
 const mutations = {
     setAuthenticatedUser: function (state, user) {
-        if (Object.keys(user).length === 0 && user.constructor === Object) {
-            localStorage.removeItem('user')
-            state.user = false
-        } else {
-            localStorage.setItem('user', JSON.stringify(user));
-            state.user = user
-        }
+        localStorage.setItem('user', JSON.stringify(user));
+        state.user = user
+    },
+
+    removeAuthenticatedUser: function (state) {
+        localStorage.removeItem('user')
+        state.user = false
     },
 }
+
+
 export default {
     namespaced: true,
     state,
